c6a915ca7b4570a2529a221f24b04edae993ded0
Ian Sealy <git@iansealy.com>
2023-09-12 23:17:29 +0100

    Exercise 1.4

--- pcost.py
+++ pcost.py
@@ -3,18 +3,21 @@
 import sys
 
 
-def pcost(file):
+def portfolio_cost(file):
     cost = 0.0
 
     with open(file, "r") as f:
         for line in f:
             name, shares, price = line.split()
-            cost += int(shares) * float(price)
+            try:
+                cost += int(shares) * float(price)
+            except ValueError as e:
+                print("Couldn't parse: %s\nReason: %s" % (repr(line), e))
 
-    print(cost)
+    return cost
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         raise SystemExit("Usage: python pcost.py file")
-    pcost(sys.argv[1])
+    print(portfolio_cost(sys.argv[1]))
