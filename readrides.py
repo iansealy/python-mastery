68ec034ea62a8e924d58f4351be313ef902184aa
Ian Sealy <git@iansealy.com>
2023-09-18 17:07:50 +0100

    Exercise 2.5

--- readrides.py
+++ readrides.py
@@ -1,5 +1,6 @@
 # readrides.py
 
+import collections.abc
 import csv
 
 
@@ -25,7 +26,7 @@ def read_rides_as_dicts(filename):
     """
     Read the bus ride data as a list of dictionaries
     """
-    records = []
+    records = RideData()
     with open(filename) as f:
         rows = csv.reader(f)
         _headings = next(rows)  # Skip headers
@@ -120,6 +121,65 @@ def read_rides_as_objects_with_slots(filename):
     return records
 
 
+def read_rides_as_columns(filename):
+    """
+    Read the bus ride data into 4 lists, representing columns
+    """
+    routes = []
+    dates = []
+    daytypes = []
+    numrides = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        _headings = next(rows)  # Skip headers
+        for row in rows:
+            routes.append(row[0])
+            dates.append(row[1])
+            daytypes.append(row[2])
+            numrides.append(int(row[3]))
+    return dict(routes=routes, dates=dates, daytypes=daytypes, numrides=numrides)
+
+
+class RideData(collections.abc.Sequence):
+    def __init__(self):
+        # Each value is a list with all of the values (a column)
+        self.routes = []
+        self.dates = []
+        self.daytypes = []
+        self.numrides = []
+
+    def __len__(self):
+        # All lists assumed to have the same length
+        return len(self.routes)
+
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            return {
+                "route": self.routes[index],
+                "date": self.dates[index],
+                "daytype": self.daytypes[index],
+                "rides": self.numrides[index],
+            }
+        elif isinstance(index, slice):
+            slice_output = []
+            for idx in range(*index.indices(len(self.routes))):
+                slice_output.append(
+                    {
+                        "route": self.routes[idx],
+                        "date": self.dates[idx],
+                        "daytype": self.daytypes[idx],
+                        "rides": self.numrides[idx],
+                    }
+                )
+            return slice_output
+
+    def append(self, d):
+        self.routes.append(d["route"])
+        self.dates.append(d["date"])
+        self.daytypes.append(d["daytype"])
+        self.numrides.append(d["rides"])
+
+
 if __name__ == "__main__":
     import tracemalloc
 
@@ -152,4 +212,9 @@ if __name__ == "__main__":
         "Memory Use: Current %d, Peak %d (object with slots)"
         % tracemalloc.get_traced_memory()
     )
+    tracemalloc.stop()
+
+    tracemalloc.start()
+    rows = read_rides_as_columns("Data/ctabus.csv")
+    print("Memory Use: Current %d, Peak %d (columns)" % tracemalloc.get_traced_memory())
     tracemalloc.stop()
