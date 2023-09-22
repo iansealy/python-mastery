860ba55df089194f1ff6e6108e8c865a7cd8ffbc
Ian Sealy <git@iansealy.com>
2023-09-22 22:36:51 +0100

    Exercise 3.4

--- stock.py
+++ stock.py
@@ -1,16 +1,43 @@
 class Stock:
-    types = (str, int, float)
+    __slots__ = ("name", "_shares", "_price")
+
+    _types = (str, int, float)
 
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
 
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, self._types[1]):
+            raise TypeError("Expected a " + self._types[1].__name__)
+        if value < 0:
+            raise ValueError("shares must be >= 0")
+        self._shares = value
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, self._types[2]):
+            raise TypeError("Expected a " + self._types[2].__name__)
+        if value < 0:
+            raise ValueError("price must be >= 0")
+        self._shares = value
+
     @classmethod
     def from_row(cls, row):
-        values = [func(val) for func, val in zip(cls.types, row)]
+        values = [func(val) for func, val in zip(cls._types, row)]
         return cls(*values)
 
+    @property
     def cost(self):
         return self.shares * self.price
 
