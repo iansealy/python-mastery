2a7a994fb84d5e284be002177af1822a4c8fdf33
Ian Sealy <git@iansealy.com>
2023-09-27 23:46:41 +0100

    Exercise 4.2

---
+++ validate.py
@@ -0,0 +1,112 @@
+# validate.py
+class Validator:
+    @classmethod
+    def check(cls, value):
+        return value
+
+
+class Typed(Validator):
+    expected_type = object
+
+    @classmethod
+    def check(cls, value):
+        if not isinstance(value, cls.expected_type):
+            raise TypeError(f"Expected {cls.expected_type}")
+        return super().check(value)
+
+
+class Integer(Typed):
+    expected_type = int
+
+
+class Float(Typed):
+    expected_type = float
+
+
+class String(Typed):
+    expected_type = str
+
+
+class Positive(Validator):
+    @classmethod
+    def check(cls, value):
+        if value < 0:
+            raise ValueError("Expected >= 0")
+        return super().check(value)
+
+
+class NonEmpty(Validator):
+    @classmethod
+    def check(cls, value):
+        if len(value) == 0:
+            raise ValueError("Must be non-empty")
+        return super().check(value)
+
+
+class PositiveInteger(Integer, Positive):
+    pass
+
+
+class PositiveFloat(Float, Positive):
+    pass
+
+
+class NonEmptyString(String, NonEmpty):
+    pass
+
+
+class Stock:
+    __slots__ = ("_name", "_shares", "_price")
+
+    _types = (str, int, float)
+
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = NonEmptyString.check(value)
+
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        self._shares = PositiveInteger.check(value)
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        self._price = PositiveFloat.check(value)
+
+    @classmethod
+    def from_row(cls, row):
+        values = [func(val) for func, val in zip(cls._types, row)]
+        return cls(*values)
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self.name!r}, {self.shares!r}, {self.price!r})"
+
+    def __eq__(self, other):
+        return isinstance(other, Stock) and (
+            (self.name, self.shares, self.price)
+            == (other.name, other.shares, other.price)
+        )
+
+    @property
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, nshares):
+        self.shares -= nshares
+        return
