0cf48f7d4ff858c86f90b296eb509737841c351d
Ian Sealy <git@iansealy.com>
2023-10-18 10:53:49 +0100

    Exercise 7.3

--- stock.py
+++ stock.py
@@ -1,16 +1,17 @@
 # stock.py
+
 from structure import Structure
+from validate import PositiveFloat, PositiveInteger, String
 
 
 class Stock(Structure):
-    _fields = ("name", "shares", "price")
+    name = String()
+    shares = PositiveInteger()
+    price = PositiveFloat()
 
     @property
     def cost(self):
         return self.shares * self.price
 
-    def sell(self, nshares):
+    def sell(self, nshares: PositiveInteger):
         self.shares -= nshares
-
-
-Stock.create_init()
--- structure.py
+++ structure.py
@@ -1,8 +1,12 @@
 # structure.py
 import sys
 
+from validate import Validator, validated
+
 
 class Structure:
+    _types = ()
+
     @staticmethod
     def _init():
         locs = sys._getframe(1).f_locals
@@ -19,6 +23,10 @@ class Structure:
         return f"{type(self).__name__}({self.name!r}, {self.shares!r}, {self.price!r})"
 
     @classmethod
+    def __init_subclass__(cls):
+        validate_attributes(cls)
+
+    @classmethod
     def create_init(cls):
         argstr = ",".join(cls._fields)
         code = f"def __init__(self, {argstr}):\n"
@@ -27,3 +35,29 @@ class Structure:
         locs = {}
         exec(code, locs)
         cls.__init__ = locs["__init__"]
+
+    @classmethod
+    def from_row(cls, row):
+        rowdata = [func(val) for func, val in zip(cls._types, row)]
+        return cls(*rowdata)
+
+
+def validate_attributes(cls):
+    validators = []
+    types = []
+    for name, val in vars(cls).items():
+        if isinstance(val, Validator):
+            validators.append(val)
+            if hasattr(val, "expected_type"):
+                types.append(val.expected_type)
+        elif hasattr(val, "__annotations__"):
+            for name2, val2 in val.__annotations__.items():
+                if issubclass(val2, Validator):
+                    setattr(cls, name, validated(val))
+
+    cls._fields = [val.name for val in validators]
+    cls._types = types
+
+    cls.create_init()
+
+    return cls
